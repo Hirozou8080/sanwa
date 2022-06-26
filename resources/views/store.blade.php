0e968c570e7d4389a8910c8140b829dd7a736405
h_kinjo <h.kinjo.11.8@gmail.com>
2022-06-26 19:54:30 +0900

    パンくずリストライブラリ追加

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 <main class="main store">
-  <div class="">
-
-  </div>
+  <section class="page-title">
+      <div class="wrapper">
+        <div class="title">店舗一覧<br><small>STORE</small></div>
+      </div>
+  </section>
+  {{ Breadcrumbs::render('store') }}
 
 
 </main>
@@ -12,27 +15,6 @@
 
 @section('script')
 <script>
-$('.slider-top').slick({
-    centerMode: true,
-    autoplay: true,         //自動再生
-	  autoplaySpeed: 5000,    //自動再生のスピード
-	  speed: 800,	            //スライドするスピード
-	  dots: true,             //スライドしたのドット
-	  arrows: true,           //左右の矢印
-	  infinite: true,         //スライドのループ
-	  pauseOnHover: false,    //ホバーしたときにスライドを一時停止しない　
-    centerPadding: '20%',
-    slidesToShow: 1,
-    responsive: [
-      {
-        breakpoint: 400, // 399px以下のサイズに適用
-        settings: {
-          slidesToShow: 1,
-          centerPadding: '0%',
-        },
-      },
-    ],
-  });
  
 </script>
 @endsection
