6997692a3d2a11b5c05044a9223f89dee872c1d3
h_kinjo <h.kinjo.11.8@gmail.com>
2022-06-27 23:57:53 +0900

    add store list table

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -9,6 +9,29 @@
   </section>
   {{ Breadcrumbs::render('store')}}
 
+  <section class="container">
+    <div class="wrapper">
+      <div class="head">
+        <div class="head-title">店舗一覧</div>
+      </div>
+      <div class="content">
+        <table>
+          <tr>
+            <th>店名</th>
+            <th>住所</th>
+            <th>サービス案内</th>
+            <th>求人案内</th>
+          </tr>
+          <tr>
+            <td>aaa</td>
+            <td>aaa</td>
+            <td>aaa</td>
+            <td>aaa</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </section>
 
 </main>
 @endsection
