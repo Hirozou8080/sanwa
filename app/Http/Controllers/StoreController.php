d60582d9cdf8787a61f484cec332baa8f1bb99f8
h_kinjo <h.kinjo.11.8@gmail.com>
2022-06-28 23:50:30 +0900

    deta add table

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 class StoreController extends Controller
 {
     public function index(){
-        return view("store");
+
+        $stores = [
+            0=>[
+            'name' => '金親本店',
+            'post_code' => '264-0002',
+            'prefectures' => '千葉県',
+            'address' => '千葉市若葉区千城台東2-24-10',
+            'service' => [
+                '土日祝営業','衣類リフォーム','駐車場あり','当日仕上げ可能'
+            ],
+            'job_offer' => true,
+            ]
+        ];
+        // dd($stores);
+        return view("store")->with('stores',$stores);
     }
 }
--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -15,7 +15,8 @@
         <div class="head-title">店舗一覧</div>
       </div>
       <div class="content">
-        <table>
+        {{-- pc用 --}}
+        <table class="pc">
           <tr>
             <th>店名</th>
             <th>住所</th>
@@ -23,11 +24,51 @@
             <th>求人案内</th>
           </tr>
           <tr>
-            <td>aaa</td>
-            <td>aaa</td>
-            <td>aaa</td>
-            <td>aaa</td>
+          @foreach ($stores as $store)
+            <td>{{ $store['name'] }} </td>
+            <td>
+              〒{{ $store['post_code'] }}<br>
+              {{ $store['prefectures'] . $store['address'] }} 
+            </td>
+            <td>
+            @foreach ($store['service'] as $service)
+            {{ $service }} ,  
+            @endforeach
+            </td>
+            <td>{{ $store['job_offer'] ? '募集中': ''  }}</td>
+            @endforeach
           </tr>
+        </table>
+
+        {{-- sp用 --}}
+        <table class="sp">
+          @foreach ($stores as $store)
+            <a href="#">
+              <tr>
+                <th>店名</th>
+                <td>{{ $store['name'] }} </td>
+              </tr>
+              <tr>
+                <th>住所</th>
+                <td>
+                  〒{{ $store['post_code'] }}<br>
+                  {{ $store['prefectures'] . $store['address'] }} 
+                </td>
+              </tr>
+              <tr>
+                <th>サービス案内</th>
+                <td>
+                  @foreach ($store['service'] as $service)
+                    {{ $service }} ,  
+                  @endforeach
+                </td>
+              </tr>
+              <tr>
+                <th>求人案内</th>
+                <td>{{ $store['job_offer'] ? '募集中': ''  }}</td>
+              </tr>
+            </a>
+          @endforeach
         </table>
       </div>
     </div>
