97851567090c2496cd9b2df8cb6d5321896852b6
h_kinjo <h.kinjo.11.8@gmail.com>
2022-06-29 23:42:20 +0900

    jquery location link

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -17,13 +17,13 @@
       <div class="content">
         {{-- pc用 --}}
         <table class="pc">
-          <tr>
+          <tr data-location-href="store">
             <th>店名</th>
             <th>住所</th>
             <th>サービス案内</th>
             <th>求人案内</th>
           </tr>
-          <tr>
+          <tr data-location-href="store">
           @foreach ($stores as $store)
             <td>{{ $store['name'] }} </td>
             <td>
@@ -43,23 +43,23 @@
         {{-- sp用 --}}
         <table class="sp">
           @foreach ($stores as $store)
-            <a href="#">
+            <div class="table-link" data-location-href="store">
               <tr>
                 <th>店名</th>
                 <td>{{ $store['name'] }} </td>
               </tr>
+              <th>住所</th>
+              <td>
+                〒{{ $store['post_code'] }}<br>
+                {{ $store['prefectures'] . $store['address'] }} 
+              </td>
               <tr>
-                <th>住所</th>
-                <td>
-                  〒{{ $store['post_code'] }}<br>
-                  {{ $store['prefectures'] . $store['address'] }} 
-                </td>
               </tr>
               <tr>
                 <th>サービス案内</th>
                 <td>
                   @foreach ($store['service'] as $service)
-                    {{ $service }} ,  
+                  {{ $service }} ,  
                   @endforeach
                 </td>
               </tr>
@@ -67,8 +67,8 @@
                 <th>求人案内</th>
                 <td>{{ $store['job_offer'] ? '募集中': ''  }}</td>
               </tr>
-            </a>
-          @endforeach
+            </div>
+            @endforeach
         </table>
       </div>
     </div>
@@ -79,6 +79,10 @@
 
 @section('script')
 <script>
- 
+  $('table tr').on('click',function(){
+    var a = $(this).data('location-href');
+    console.log(a);
+    // window.location.href = '/'+a;
+  });
 </script>
 @endsection
