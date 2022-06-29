9a6f02a921a42d8b61ded801e5d2fe121df6dccb
h_kinjo <h.kinjo.11.8@gmail.com>
2022-06-29 23:51:11 +0900

    store table edit

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -16,62 +16,72 @@
       </div>
       <div class="content">
         {{-- pc用 --}}
-        <table class="pc">
-          <tr data-location-href="store">
-            <th>店名</th>
-            <th>住所</th>
-            <th>サービス案内</th>
-            <th>求人案内</th>
-          </tr>
-          <tr data-location-href="store">
-          @foreach ($stores as $store)
-            <td>{{ $store['name'] }} </td>
-            <td>
-              〒{{ $store['post_code'] }}<br>
-              {{ $store['prefectures'] . $store['address'] }} 
-            </td>
-            <td>
-            @foreach ($store['service'] as $service)
-            {{ $service }} ,  
-            @endforeach
-            </td>
-            <td>{{ $store['job_offer'] ? '募集中': ''  }}</td>
-            @endforeach
-          </tr>
-        </table>
-
-        {{-- sp用 --}}
-        <table class="sp">
-          @foreach ($stores as $store)
-            <div class="table-link" data-location-href="store">
-              <tr>
+        <div class="pc">
+          <table>
+            <div class="store-block">
+              <tr data-location-href="store">
                 <th>店名</th>
-                <td>{{ $store['name'] }} </td>
-              </tr>
-              <th>住所</th>
-              <td>
-                〒{{ $store['post_code'] }}<br>
-                {{ $store['prefectures'] . $store['address'] }} 
-              </td>
-              <tr>
-              </tr>
-              <tr>
+                <th>住所</th>
                 <th>サービス案内</th>
-                <td>
-                  @foreach ($store['service'] as $service)
-                  {{ $service }} ,  
-                  @endforeach
-                </td>
-              </tr>
-              <tr>
                 <th>求人案内</th>
+              </tr>
+              <tr data-location-href="store">
+              @foreach ($stores as $store)
+                <td>{{ $store['name'] }} </td>
+                <td>
+                  〒{{ $store['post_code'] }}<br>
+                  {{ $store['prefectures'] . $store['address'] }} 
+                </td>
+                <td>
+                @foreach ($store['service'] as $service)
+                {{ $service }} ,  
+                @endforeach
+                </td>
                 <td>{{ $store['job_offer'] ? '募集中': ''  }}</td>
+                @endforeach
               </tr>
             </div>
-            @endforeach
-        </table>
+          </table>
+        </div>
+        {{-- ./pc --}}
+
+        {{-- sp用 --}}
+        <div class="sp">
+          <table>
+            @foreach ($stores as $store)
+              <div class="table-link" data-location-href="store">
+                <tr>
+                  <th>店名</th>
+                  <td>{{ $store['name'] }} </td>
+                </tr>
+                <th>住所</th>
+                <td>
+                  〒{{ $store['post_code'] }}<br>
+                  {{ $store['prefectures'] . $store['address'] }} 
+                </td>
+                <tr>
+                </tr>
+                <tr>
+                  <th>サービス案内</th>
+                  <td>
+                    @foreach ($store['service'] as $service)
+                    {{ $service }} ,  
+                    @endforeach
+                  </td>
+                </tr>
+                <tr>
+                  <th>求人案内</th>
+                  <td>{{ $store['job_offer'] ? '募集中': ''  }}</td>
+                </tr>
+              </div>
+              @endforeach
+          </table>
+        </div>
+        {{--./ pc --}}
       </div>
+      {{-- ./content --}}
     </div>
+    {{-- ./wrapper --}}
   </section>
 
 </main>
