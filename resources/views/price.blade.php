326fd06191f729704a1b6363dfeffbc2d9c8a0d9
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-02 14:03:25 +0900

    [sp] desine fix

--- resources/views/price.blade.php
+++ resources/views/price.blade.php
@@ -11,7 +11,25 @@
   <section class="container">
     <div class="wrapper">
       <div class="head">
-        <div class="head-title">店舗一覧（{{count($stores)}}件）</div>
+        <div class="head-title">店舗一覧（{{count($stores)}}店舗）</div>
+        <div class="txt">
+          店舗によって料金の異なる場合がございます。<br>
+          お客様のご希望に寄った店舗へお越しください。
+        </div>
+      </div>
+      <div class="content">
+        @foreach ( $stores as $store)
+          <div class="block">
+            <div class="block-head">
+              {{$store['name']}}
+            </div>
+            <div class="block-body">
+              <div class="price-link">
+                <a href="#">料金表を見る</a>
+              </div>
+            </div>
+          </div>
+          @endforeach
       </div>
     </div>
     {{-- ./wrapper --}}
