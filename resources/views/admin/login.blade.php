521e92b85f4ec79f58e4d361dfa8a841d3f15f2f
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-07 23:38:38 +0900

    login page edit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,7 +2,16 @@
 
 @section('content')
 <main class="main login">
-  ログイン画面
+  <div class="wrapper">
+    <div class="container">
+      <div class="head">
+        ログイン
+      </div>
+      <form action="">
+
+      </form>
+    </div>
+  </div>
 </main>
 @endsection
 
