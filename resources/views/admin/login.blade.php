d7a2264d9794a12d9f93579f7ee24dc87ea21efe
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-11 00:08:46 +0900

    login layout edit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,33 +2,35 @@
 @section('css','login')
 @section('content')
 <main class="main login">
-  <div class="container">
-    <div class="head">
-      ログイン
-      <hr>
-    </div>
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-          @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-          @endforeach
-        </ul>
+  <div class="wrapper">
+    <div class="container">
+      <div class="head">
+        ログイン
+        <hr>
       </div>
-    @endif
-    
-    <div class="content">
-      {{ Form::open([ 'url'=>route("login") ,'class'=>'login-form', 'method'=>'post' ]) }}
-      <div class="form-item">
-        {{ Form::text('email', null, ['class' => 'form-control', 'id' => 'email', 'placeholder' => 'メールアドレス'])}}
+      @if ($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
+      
+      <div class="content">
+        {{ Form::open([ 'url'=>route("login") ,'class'=>'login-form', 'method'=>'post' ]) }}
+        <div class="form-item">
+          {{ Form::text('email', null, ['class' => 'form-control', 'id' => 'email', 'placeholder' => 'メールアドレス'])}}
+        </div>
+        <div class="form-item">
+          {{ Form::password('password', ['class' => 'form-control','id' => 'password','placeholder' => 'パスワード'])}}
+        </div>
+        <div class="form-item">
+          {{ Form::submit('ログイン', ['class' => 'btn btn-primary']) }}
+        </div>
+        {{ Form::close() }}
       </div>
-      <div class="form-item">
-        {{ Form::password('password', ['class' => 'form-control','id' => 'password','placeholder' => 'パスワード'])}}
-      </div>
-      <div class="form-item">
-        {{ Form::submit('ログイン', ['class' => 'btn btn-primary']) }}
-      </div>
-      {{ Form::close() }}
     </div>
   </div>
 </main>
