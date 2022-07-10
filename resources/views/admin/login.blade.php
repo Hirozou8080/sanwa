eea6d0e5d2d9545200a526230821c1003d726db5
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-10 23:53:35 +0900

    login view desine

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,32 +2,33 @@
 @section('css','login')
 @section('content')
 <main class="main login">
-  <div class="wrapper">
-    <div class="container">
-      <div class="head">
-        ログイン
+  <div class="container">
+    <div class="head">
+      ログイン
+      <hr>
+    </div>
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
       </div>
-      @if ($errors->any())
-        <div class="alert alert-danger">
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-      @endif
-      
-      <div class="content">
-        {{ Form::open([ 'url'=>route("login") ,'class'=>'login-form', 'method'=>'post' ]) }}
-        <div class="form-item">
-          {{Form::text('email', null, ['class' => 'form-control', 'id' => 'email', 'placeholder' => 'メールアドレス'])}}
-        </div>
-        <div class="form-item">
-          {{Form::password('password', ['class' => 'form-control','id' => 'password','placeholder' => 'パスワード'])}}
-        </div>
-        {{ Form::submit('送信', ['class' => 'btn btn-primary']) }}
-        {{ Form::close() }}
+    @endif
+    
+    <div class="content">
+      {{ Form::open([ 'url'=>route("login") ,'class'=>'login-form', 'method'=>'post' ]) }}
+      <div class="form-item">
+        {{ Form::text('email', null, ['class' => 'form-control', 'id' => 'email', 'placeholder' => 'メールアドレス'])}}
       </div>
+      <div class="form-item">
+        {{ Form::password('password', ['class' => 'form-control','id' => 'password','placeholder' => 'パスワード'])}}
+      </div>
+      <div class="form-item">
+        {{ Form::submit('ログイン', ['class' => 'btn btn-primary']) }}
+      </div>
+      {{ Form::close() }}
     </div>
   </div>
 </main>
--- resources/views/admin/tmpl/footer.blade.php
+++ resources/views/admin/tmpl/footer.blade.php
@@ -1,7 +1,5 @@
 <footer class="footer">
   <div class="wrapper">
-    <div class="content">
-    </div>
       <div class="author">© 2021 Hiroki-Kinjo</div>
   </div>
 </footer>
