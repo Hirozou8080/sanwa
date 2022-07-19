c8fd98e14ad65c4a65a1e5fc40f4d6563cbb2327
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-19 22:20:59 +0900

    Create Store Table

---
+++ database/migrations/2022_07_18_173112_create_stores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id')->comment('ID');
+            $table->string('name')->comment('店舗名');
+            $table->integer('post_num')->comment('郵便番号');
+            $table->string('address')->comment('住所');
+            $table->boolean('recruit_flg')->nullable()->comment('求人募集フラグ');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    // ALTER 文を実行しテーブルにコメントを設定
+    DB::statement("ALTER TABLE stores COMMENT '店舗テーブル'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+};
