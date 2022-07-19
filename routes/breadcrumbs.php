f132901b271307c1c63adb4bd3b346bbee117e3d
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-19 22:20:22 +0900

    breadcrumps name change

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -10,7 +10,7 @@ Breadcrumbs::for('store', function ($trail) {
     $trail->parent('home');
     $trail->push('店舗一覧', route('store'));
 });
-// Home > Store
+// Home > Store > Price
 Breadcrumbs::for('price', function ($trail) {
     $trail->parent('home');
     $trail->push('料金一覧', route('price'));
