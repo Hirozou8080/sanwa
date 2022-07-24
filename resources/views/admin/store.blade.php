255f0ca99a2d61127fb6afd42db8c58048b79f59
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-24 23:40:07 +0900

    store up onece

--- resources/views/admin/store.blade.php
+++ resources/views/admin/store.blade.php
@@ -12,6 +12,11 @@
 <div class="content">
   <section class="pages">
     <div class="pages-inner">
+      <div class="list-head">
+        <div class="link-btn">
+          <a href="#">登録＞</a>
+        </div>
+      </div>
       <table class="list">
         <tr>
           <th>ID</th>
@@ -19,7 +24,12 @@
           <th>場所</th>
           <th></th>
         </tr>
-
+        <tr>
+          <td>1</td>
+          <td>金親</td>
+          <td>千葉県千葉市若葉区</td>
+          <td>編集ボタン</td>
+        </tr>
       </table>
     </div>
   </section>
