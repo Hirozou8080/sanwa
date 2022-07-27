2677233656997fea3a48e02f9eea8341603745a9
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-27 23:57:38 +0900

    store wip

--- resources/views/admin/store/add.blade.php
+++ resources/views/admin/store/add.blade.php
@@ -12,7 +12,32 @@
 <div class="content">
   <section class="pages">
     <div class="pages-inner">
-      
+      <form action="{{route('admin/store/add')}}" method="post">
+        <div>
+          <input type="text" placeholder="店舗名">
+        </div>
+
+        <div>
+          <input type="text" placeholder="郵便番号">
+        </div>
+
+        <div>
+          <input type="text" placeholder="都道府県">
+        </div>
+
+        <div>
+          <input type="text" placeholder="市区町村">
+        </div>
+
+        <div>
+          <input type="text" placeholder="住所">
+        </div>
+        
+        <input type="checkbox" for="yes">
+          <label  id="yes">あり<label>
+          <input type="checkbox" for="no">
+        <label  id="no">なし<label>
+      </form>
     </div>
   </section>
 </div>
