7873fdabf6a4be29c8ddd3431b7a91fd300bd917
h_kinjo <h.kinjo.11.8@gmail.com>
2022-07-28 23:50:46 +0900

    store wip

--- resources/views/admin/store/add.blade.php
+++ resources/views/admin/store/add.blade.php
@@ -1,50 +1,52 @@
 @extends('admin.tmpl.layouts')
-@section('css','store')
-@section('pageClassName','store-add')
-@section('pageTitle','店舗設定')
+@section('css', 'store')
+@section('pageClassName', 'store-add')
+@section('pageTitle', '店舗設定')
 @section('content')
-<div class="head">
-  <div class="title">
-    店舗登録
-  </div>
-</div>
-
-<div class="content">
-  <section class="pages">
-    <div class="pages-inner">
-      <form action="{{route('admin/store/add')}}" method="post">
-        <div>
-          <input type="text" placeholder="店舗名">
-        </div>
-
-        <div>
-          <input type="text" placeholder="郵便番号">
-        </div>
-
-        <div>
-          <input type="text" placeholder="都道府県">
-        </div>
-
-        <div>
-          <input type="text" placeholder="市区町村">
-        </div>
-
-        <div>
-          <input type="text" placeholder="住所">
-        </div>
-        
-        <input type="checkbox" for="yes">
-          <label  id="yes">あり<label>
-          <input type="checkbox" for="no">
-        <label  id="no">なし<label>
-      </form>
+  <div class="head">
+    <div class="title">
+      店舗登録
     </div>
-  </section>
-</div>
+  </div>
+
+  <div class="content">
+    <section class="pages">
+      <div class="pages-inner">
+        <div class="head">
+          新しい店舗を登録
+        </div>
+        <div class="form-control">
+          {{Form::open(['url' => route('admin/store/add') , 'files' => true])}}
+            <div class="form-item">
+              {{Form::label($for="store_nama",'店舗名')}}
+              {{Form::text('store_nama', null, ['class' => 'form-control', 'id' => 'storeName', 'placeholder' => '店舗名'])}}
+            </div>
+            <div class="form-item">
+              {{Form::label($for="postNum",'郵便番号')}}
+              {{Form::text('postNum', null, ['class' => 'form-control', 'id' => 'postNum', 'placeholder' => '郵便番号'])}}
+            </div>
+            <div class="form-item">
+              <input type="text" placeholder="都道府県">
+            </div>
+            <div class="form-item">
+              <input type="text" placeholder="市区町村">
+            </div>
+            <div class="form-item">
+              <input type="text" placeholder="住所">
+            </div>
+            <div class="form-item">
+              <label id="yes">あり<label>
+                  <input type="checkbox" for="yes">
+              <label id="no">なし<label>
+                  <input type="checkbox" for="no">
+            </div>
+          {{Form::close()}}
+        </div>
+      </div>
+    </section>
+  </div>
 @endsection
 
 @section('script')
-<script>
- 
-</script>
+  <script></script>
 @endsection
--- resources/views/admin/store/list.blade.php
+++ resources/views/admin/store/list.blade.php
@@ -1,43 +1,41 @@
 @extends('admin.tmpl.layouts')
-@section('css','store')
-@section('pageClassName','store')
-@section('pageTitle','店舗設定')
+@section('css', 'store')
+@section('pageClassName', 'store')
+@section('pageTitle', '店舗設定')
 @section('content')
-<div class="head">
-  <div class="title">
-    店舗一覧
-  </div>
-</div>
-
-<div class="content">
-  <section class="pages">
-    <div class="pages-inner">
-      <div class="list-head">
-        <div class="link-btn">
-          <a href={{route('admin/store/add')}}>登録＞</a>
-        </div>
-      </div>
-      <table class="list">
-        <tr>
-          <th>ID</th>
-          <th>店舗名</th>
-          <th>場所</th>
-          <th></th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>金親</td>
-          <td>千葉県千葉市若葉区</td>
-          <td>編集ボタン</td>
-        </tr>
-      </table>
+  <div class="head">
+    <div class="title">
+      店舗一覧
     </div>
-  </section>
-</div>
+  </div>
+
+  <div class="content">
+    <section class="pages">
+      <div class="pages-inner">
+        <div class="list-head">
+          <div class="link-btn">
+            <a href={{ route('admin/store/add') }}>登録＞</a>
+          </div>
+        </div>
+        <table class="list">
+          <tr>
+            <th>ID</th>
+            <th>店舗名</th>
+            <th>場所</th>
+            <th></th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>金親</td>
+            <td>千葉県千葉市若葉区</td>
+            <td>編集ボタン</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+  </div>
 @endsection
 
 @section('script')
-<script>
- 
-</script>
+  <script></script>
 @endsection
