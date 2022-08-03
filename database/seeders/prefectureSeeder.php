61dbb1b534dce7ed1f5b3ec3c2e51f375df3cc8b
h_kinjo <h.kinjo.11.8@gmail.com>
2022-08-03 23:35:07 +0900

    create prefectures table

---
+++ database/seeders/prefectureSeeder.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Prefecture;
+
+class prefectureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        Prefecture::create([
+        'name'=>'北海道' // 1
+        ]);
+        Prefecture::create([
+            'name'=>'青森県' // 2
+        ]);
+        Prefecture::create([
+            'name'=>'岩手県' // 3
+        ]);
+        Prefecture::create([
+            'name'=>'宮城県' // 4
+        ]);
+        Prefecture::create([
+            'name'=>'秋田県' // 5
+        ]);
+        Prefecture::create([
+            'name'=>'山形県' // 6
+        ]);
+        Prefecture::create([
+            'name'=>'福島県' // 7
+        ]);
+        Prefecture::create([
+            'name'=>'茨城県' // 8
+        ]);
+        Prefecture::create([
+            'name'=>'栃木県' // 9
+        ]);
+        Prefecture::create([
+            'name'=>'群馬県' // 10
+        ]);
+        Prefecture::create([
+            'name'=>'埼玉県' // 11
+        ]);
+        Prefecture::create([
+            'name'=>'千葉県' // 12
+        ]);
+        Prefecture::create([
+            'name'=>'東京都' // 13
+        ]);
+        Prefecture::create([
+            'name'=>'神奈川県' // 14
+        ]);
+        Prefecture::create([
+            'name'=>'新潟県' // 15
+        ]);
+        Prefecture::create([
+            'name'=>'富山県' // 16
+        ]);
+        Prefecture::create([
+            'name'=>'石川県' // 17
+        ]);
+        Prefecture::create([
+            'name'=>'福井県' // 18
+        ]);
+        Prefecture::create([
+            'name'=>'山梨県' // 19
+        ]);
+        Prefecture::create([
+            'name'=>'長野県' // 20
+        ]);
+        Prefecture::create([
+            'name'=>'岐阜県' // 21
+        ]);
+        Prefecture::create([
+            'name'=>'静岡県' // 22
+        ]);
+        Prefecture::create([
+            'name'=>'愛知県' // 23
+        ]);
+        Prefecture::create([
+            'name'=>'三重県' // 24
+        ]);
+        Prefecture::create([
+            'name'=>'滋賀県' // 25
+        ]);
+        Prefecture::create([
+            'name'=>'京都府' // 26
+        ]);
+        Prefecture::create([
+            'name'=>'大阪府' // 27
+        ]);
+        Prefecture::create([
+            'name'=>'兵庫県' // 28
+        ]);
+        Prefecture::create([
+            'name'=>'奈良県' // 29
+        ]);
+        Prefecture::create([
+            'name'=>'和歌山県' // 30
+        ]);
+        Prefecture::create([
+            'name'=>'鳥取県' // 31
+        ]);
+        Prefecture::create([
+            'name'=>'島根県' // 32
+        ]);
+        Prefecture::create([
+            'name'=>'岡山県' // 33
+        ]);
+        Prefecture::create([
+            'name'=>'広島県' // 34
+        ]);
+        Prefecture::create([
+            'name'=>'山口県' // 35
+        ]);
+        Prefecture::create([
+            'name'=>'徳島県' // 36
+        ]);
+        Prefecture::create([
+            'name'=>'香川県' // 37
+        ]);
+        Prefecture::create([
+            'name'=>'愛媛県' // 38
+        ]);
+        Prefecture::create([
+            'name'=>'高知県' // 39
+        ]);
+        Prefecture::create([
+            'name'=>'福岡県' // 40
+        ]);
+        Prefecture::create([
+            'name'=>'佐賀県' // 41
+        ]);
+        Prefecture::create([
+            'name'=>'長崎県' // 42
+        ]);
+        Prefecture::create([
+            'name'=>'熊本県' // 43
+        ]);
+        Prefecture::create([
+            'name'=>'大分県' // 44
+        ]);
+        Prefecture::create([
+            'name'=>'宮崎県' // 45
+        ]);
+        Prefecture::create([
+            'name'=>'鹿児島県' // 46
+        ]);
+        Prefecture::create([
+            'name'=>'沖縄県' // 47
+        ]);
+    }
+}
