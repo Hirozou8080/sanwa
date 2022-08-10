66ffa79a31880ddc1b6bb0e86325f9c158b0cf30
h_kinjo <h.kinjo.11.8@gmail.com>
2022-08-11 00:13:28 +0900

    store regist update

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -80,7 +80,7 @@ return [
     'mimes' => 'The :attribute must be a file of type: :values.',
     'mimetypes' => 'The :attribute must be a file of type: :values.',
     'min' => [
-        'numeric' => 'The :attribute must be at least :min.',
+        'numeric' => ':attribute は :min文字以上で入力してください。',
         'file' => 'The :attribute must be at least :min kilobytes.',
         'string' => 'The :attribute must be at least :min characters.',
         'array' => 'The :attribute must have at least :min items.',
