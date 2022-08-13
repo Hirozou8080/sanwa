8d9e58f65ce5a2949b241dbd8839b5182978a9ec
h_kinjo <h.kinjo.11.8@gmail.com>
2022-08-13 20:54:42 +0900

    update detail page

--- resources/views/admin/store/detail.blade.php
+++ resources/views/admin/store/detail.blade.php
@@ -14,71 +14,63 @@
         <div class="head">
           店舗詳細
         </div>
-        <div class="form-block">
-          <div class="form-item">
-            <div class="form-title">
+        <div class="block">
+          <div class="item">
+            <div class="item-title">
               <div>店舗名</div>
             </div>
-            <div class="form-content">
+            <div class="item-content">
               {{$store->name}}
             </div>
           </div>
-          <div class="form-item">
-            <div class="form-title">
+          <div class="item">
+            <div class="item-title">
               郵便番号
             </div>
-            <div class=" flex-items">
-              <div class="form-content">
+              <div class="item-content">
                 {{$store->postNumPrev}}
-              </div>
-              ー
-              <div class="form-content">
+                -
                 {{$store->postNumNext}}
               </div>
-            </div>
           </div>
-          <div class="form-item">
-            <div class="form-title">
+          <div class="item">
+            <div class="item-title">
               都道府県
             </div>
-            <div class="form-content">
+            <div class="item-content">
                 {{$prefecture->name}}
             </select>
             </div>
           </div>
-          <div class="form-item">
-            <div class="form-title">
+          <div class="item">
+            <div class="item-title">
               市区町村
             </div>
-            <div class="form-content">
+            <div class="item-content">
               {{$store->city}}  
             </div>
           </div>
-          <div class="form-item">
-            <div class="form-title">
+          <div class="item">
+            <div class="item-title">
               住所
             </div>
-            <div class="form-content">
+            <div class="item-content">
               {{$store->address}}
             </div>
           </div>
-          <div class="form-item">
-            <div class="form-title">
+          <div class="item">
+            <div class="item-title">
               求人情報
             </div>
-            <div class=" flex-items" style="justify-content:center; gap:50px ; border:1px solid #ccc;border-radius:5px; padding:.5rem">
-              <div class="form-content">
-                <div class="flex-items">
-                @if($store->recruit_flg == 1)
+              <div class="item-content">
+                  @if($store->recruit_flg == 1)
                   募集中
                   @else
                   募集なし
                 @endif
-                </div>
               </div>
-            </div>
           </div>
-          <div class="form-item" style="justify-content:center; padding:3rem 1rem">
+          <div class="item" style="justify-content:center; padding:3rem 1rem">
             <div class="flex-items">
               <button type="button" onclick="location.href='{{ route('admin/store')}}' ">戻る＞</button>
             </div>
