ab92294a2a63409e4c51937fe2631f3e50391626
h_kinjo <h.kinjo.11.8@gmail.com>
2022-08-30 23:57:37 +0900

    wip

--- resources/views/admin/alert/add.blade.php
+++ resources/views/admin/alert/add.blade.php
@@ -27,10 +27,23 @@
           {{ Form::open(['url' => route('admin/alert/add'), 'files' => true]) }}
           <div class="form-item">
             <div class="form-title">
-              {{ Form::label($for = 'title', '件名') }}
+              {{ Form::label('カテゴリー') }}
+            </div>
+            <div class="flex-items" style="justify-content:flex-start; gap:50px ;  padding:.5rem;font-size:1rem:">
+              <div class="form-content">
+                <div class=" flex-items">
+                  {{ Form::checkbox('category', 1, true, ['id' => 'category_1', 'class' => 'circle']) }}
+                  {{ Form::label($for = 'category_1', '重量なお知らせ', ['class' => 'category']) }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-item">
+            <div class="form-title">
+              {{ Form::label($for = 'title', 'タイトル') }}
             </div>
             <div class="form-content">
-              {{ Form::text('title', null, ['class' => 'form-control', 'id' => 'title', 'placeholder' => 'マリンピア店']) }}
+              {{ Form::text('title', null, ['class' => 'form-control', 'id' => 'title', 'placeholder' => '〇〇について']) }}
             </div>
           </div>
           <div class="form-item">
@@ -43,16 +56,12 @@
           </div>
           <div class="form-item">
             <div class="form-title">
-              {{ Form::label('カテゴリー') }}
+              {{ Form::label('タイトル画像') }}
             </div>
-            <div class="flex-items" style="justify-content:flex-start; gap:50px ;  padding:.5rem;font-size:1rem:">
-              <div class="form-content">
-                <div class=" flex-items">
-                  {{ Form::checkbox('category', 1, true, ['id' => 'category_1', 'class' => 'circle']) }}
-                  {{ Form::label($for = 'category_1', '重量なお知らせ', ['class' => 'category']) }}
-                </div>
-              </div>
+            <div class="form-content">
+              {{ Form::file('image', ['class' => 'custom-file-input', 'id' => 'fileImage']) }}
             </div>
+            <div id="preview"></div>
           </div>
           <div class="form-item" style="justify-content:space-between; padding:3rem 1rem">
             <div class="flex-items">
@@ -68,5 +77,41 @@
 @endsection
 
 @section('script')
-  <script></script>
+  <script>
+    //fileを変更したら
+    $('#fileImage').on('change', function() {
+
+      //fileの値は空ではなくなるはず
+      if ($('#fileImage').val() !== '') {
+
+        //propを使って、file[0]にアクセスする
+        var image_ = $('#fileImage').prop('files')[0];
+
+        //添付されたのが本当に画像かどうか、ファイル名と、ファイルタイプを正規表現で検証する
+        if (!/\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/.test(image_.name) || !/(jpg|jpeg|png|gif)$/.test(image_.type)) {
+          alert('JPG、GIF、PNGファイルの画像を添付してください。');
+          //添付された画像ファイルが１M以下か検証する
+        } else if (1048576 < image_.size) {
+          alert('1MB以下の画像を添付してください。');
+        } else {
+          //window.FileReaderに対応しているブラウザどうか
+          if (window.FileReader) {
+            //FileReaderをインスタンス化
+            var reader_ = new FileReader();
+            //添付ファイルの読み込みが成功したときに実行されるイベント（成功時のみ）
+            //一旦飛ばしてreader_ .readAsDataURLが先に動く
+            reader_.onload = function() {
+              //Data URI Schemeをimgタグのsrcにいれてリアルタイムに添付画像を描画する
+              $('#preview').attr('src', reader_.result);
+            }
+            //Data URI Schemeを取得する 
+            reader_.readAsDataURL(image_);
+          }
+          return false;
+        }
+      }
+      //ダメだったら値をクリアする
+      $('#fileImage').val('');
+    });
+  </script>
 @endsection
