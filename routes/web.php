96de9992d43a558fd6d8765cccd533e91f0eb432
h_kinjo <h.kinjo.11.8@gmail.com>
2023-04-16 21:13:32 +0900

    web.php idnent fix

--- routes/web.php
+++ routes/web.php
@@ -13,49 +13,49 @@ use App\Http\Controllers\Admin\SettingController as AdminSettingController;
 use App\Http\Controllers\Admin\PriceController as AdminPriceController;
 
 Route::controller(HomeController::class)->group(function () {
-    Route::get('/', 'home')->name('home');
+  Route::get('/', 'home')->name('home');
 });
 Route::controller(StoreController::class)->group(function () {
-    Route::get('/store', 'index')->name('store');
+  Route::get('/store', 'index')->name('store');
 });
 Route::controller(PriceController::class)->group(function () {
-    Route::get('/price', 'index')->name('price');
-    Route::get('/price/{{price_id}}', 'detail')->name('price/detail');
+  Route::get('/price', 'index')->name('price');
+  Route::get('/price/{{price_id}}', 'detail')->name('price/detail');
 });
 
 
 Route::prefix('admin')->group(function () {
-    Route::controller(LoginController::class)->group(function () {
-        Route::get('/register', 'register')->name('register');
-        Route::post('/register', 'registerPost')->name('register');
-        Route::get('/login', 'login')->name('login');
-        Route::post('/login', 'loginPost')->name('login');
-        Route::get('/logout', 'logout')->name('logout');
+  Route::controller(LoginController::class)->group(function () {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/register', 'registerPost')->name('register');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'loginPost')->name('login');
+    Route::get('/logout', 'logout')->name('logout');
+  });
+  Route::group(['middleware' => ['loginCheck']], function () {
+    Route::controller(AdminController::class)->group(function () {
+      Route::get('/', 'home')->name('admin/home');
     });
-    Route::group(['middleware' => ['loginCheck']], function () {
-        Route::controller(AdminController::class)->group(function () {
-            Route::get('/', 'home')->name('admin/home');
-        });
-        Route::controller(AdminStoreController::class)->group(function () {
-            Route::get('/store', 'store')->name('admin/store');
-            Route::get('/store/add', 'add')->name('admin/store/add');
-            Route::post('/store/add', 'addPost')->name('admin/store/add');
-            Route::get('/store/edit/{store_id}', 'edit')->name('admin/store/edit');
-            Route::post('/store/edit/{store_id}', 'editPost')->name('admin/store/edit');
-            Route::get('/store/detail/{store_id}', 'detail')->name('admin/store/detail');
-            Route::post('/store/delete/{store_id}', 'deletePost')->name('admin/store/delete');
-        });
-        Route::controller(AdminPriceController::class)->group(function () {
-            Route::get('/store/price/{store_id}', 'price')->name('admin/store/price');
-        });
+    Route::controller(AdminStoreController::class)->group(function () {
+      Route::get('/store', 'store')->name('admin/store');
+      Route::get('/store/add', 'add')->name('admin/store/add');
+      Route::post('/store/add', 'addPost')->name('admin/store/add');
+      Route::get('/store/edit/{store_id}', 'edit')->name('admin/store/edit');
+      Route::post('/store/edit/{store_id}', 'editPost')->name('admin/store/edit');
+      Route::get('/store/detail/{store_id}', 'detail')->name('admin/store/detail');
+      Route::post('/store/delete/{store_id}', 'deletePost')->name('admin/store/delete');
+    });
+    Route::controller(AdminPriceController::class)->group(function () {
+      Route::get('/store/price/{store_id}', 'price')->name('admin/store/price');
+    });
 
-        Route::controller(AdminAlertController::class)->group(function () {
-            Route::get('/alert', 'alert')->name('admin/alert');
-            Route::get('/alert/add', 'add')->name('admin/alert/add');
-            Route::post('/alert/add', 'addPost')->name('admin/alert/add');
-        });
-        Route::controller(AdminSettingController::class)->group(function () {
-            Route::get('/setting', 'setting')->name('admin/setting');
-        });
+    Route::controller(AdminAlertController::class)->group(function () {
+      Route::get('/alert', 'alert')->name('admin/alert');
+      Route::get('/alert/add', 'add')->name('admin/alert/add');
+      Route::post('/alert/add', 'addPost')->name('admin/alert/add');
     });
+    Route::controller(AdminSettingController::class)->group(function () {
+      Route::get('/setting', 'setting')->name('admin/setting');
+    });
+  });
 });
