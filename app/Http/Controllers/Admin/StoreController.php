61ce41ca4142c046e0cabdc75d8656327a73fce5
h_kinjo <h.kinjo.11.8@gmail.com>
2023-04-30 19:49:22 +0900

    店舗登録修正

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -10,44 +10,49 @@ use Illuminate\Http\Request;
 class StoreController extends Controller
 {
      protected $storeService;
+     protected $commonController;
+     protected $user_id;
+
      public function __construct(StoreService $storeService)
      {
-         //ログイン認証処理
-         $this->storeService = $storeService;
+          $this->storeService = $storeService;
+          $this->commonController = new CommonController();
+          // sessionユーザIDを取得
+          $this->user_id = session()->get('user_id');
      }
-    /**
-     * 店舗一覧
-     */
-    public function store(Request $request){
-         // user取得
-         $commonController = new CommonController();
-         // sessionユーザIDを取得
-         $user_id = session()->get('user_id');
-         $user = $commonController->getUser($user_id);
-         $stores = $commonController->getAllStore();
-         $prefectures = $commonController->getAllPrefecture();
-          return view('admin.store.index',['user'=>$user,'stores'=>$stores,'prefectures'=>$prefectures]);
-    }
 
-    /**
-     * 店舗登録
-     */
-    public function add(Request $request){
-         // user取得
-         $commonController = new CommonController();
-         // sessionユーザIDを取得
-         $user_id = session()->get('user_id');
-         $user = $commonController->getUser($user_id);
-         $prefectures = $commonController->getAllPrefecture();
-         return view('admin.store.add',['user'=>$user,'prefectures'=>$prefectures]);
-    }
-    
-    /**
-     * 店舗登録
-     */
-    public function addPost(Request $request){
+     /**
+      * 店舗一覧
+      */
+     public function store()
+     {
+          $stores = $this->commonController->getAllStore();
+          $prefectures = $this->commonController->getAllPrefecture();
+
+          // user取得
+          $user = $this->commonController->getUser($this->user_id);
+          return view('admin.store.index', ['user' => $user, 'stores' => $stores, 'prefectures' => $prefectures]);
+     }
+
+     /**
+      * 店舗登録
+      */
+     public function add()
+     {
+          $prefectures = $this->commonController->getAllPrefecture();
+
+          // user取得
+          $user = $this->commonController->getUser($this->user_id);
+          return view('admin.store.add', ['user' => $user, 'prefectures' => $prefectures]);
+     }
+
+     /**
+      * 店舗登録
+      */
+     public function addPost(Request $request)
+     {
           // バリデーション
-          $validated = $request->validate([
+          $request->validate([
                'storeName' => 'required | max:36',
                'postNumPrev' => 'required | min:3 | numeric',
                'postNumNext' => 'required | min:4 | numeric',
@@ -58,63 +63,73 @@ class StoreController extends Controller
           ]);
           // 店舗登録処理
           $this->storeService->storeRegist($request);
-          // return view('admin.store.list',['user'=>$user,'stores'=>$stores]);
           return redirect()->route('admin/store');
      }
+
      /**
       * 店舗編集
+      * @param store_id  対象店舗ID
       */
-     public function edit(Request $request, $store_id){
+     public function edit($store_id)
+     {
+          $prefectures = $this->commonController->getAllPrefecture();
+          $store = $this->commonController->getStore($store_id);
+          $store['postNumPrev'] = substr($store['post_num'], 0, 3);
+          $store['postNumNext'] = substr($store['post_num'], 3, 7);
+
           // user取得
-          $commonController = new CommonController();
-          // sessionユーザIDを取得
-          $user_id = session()->get('user_id');
-          $user = $commonController->getUser($user_id);
-          $prefectures = $commonController->getAllPrefecture();
-          $store = $commonController->getStore($store_id);
-          $store['postNumPrev'] = substr($store['post_num'],0,3);
-          $store['postNumNext'] = substr($store['post_num'],3,7);
-          return view('admin.store.edit',['user'=>$user,'store'=>$store,'prefectures'=>$prefectures]);
+          $user = $this->commonController->getUser($this->user_id);
+          return view('admin.store.edit', ['user' => $user, 'store' => $store, 'prefectures' => $prefectures]);
      }
+
      /**
       * 店舗編集
+      * @param request  更新データ
+      * @param store_id  対象店舗ID
       */
-      public function editPost(Request $request, $store_id){
-           // バリデーション
-           $validated = $request->validate([
-                'storeName' => 'required | max:36',
-                'postNumPrev' => 'required | min:3 | numeric',
-                'postNumNext' => 'required | min:4 | numeric',
-                'prefecture' => 'required',
-                'city' => 'required | max:32',
-                'address' => 'required | max:64',
-                'recruit' => 'required',
+     public function editPost(Request $request, $store_id)
+     {
+          // バリデーション
+          $request->validate([
+               'storeName' => 'required | max:36',
+               'postNumPrev' => 'required | min:3 | numeric',
+               'postNumNext' => 'required | min:4 | numeric',
+               'prefecture' => 'required',
+               'city' => 'required | max:32',
+               'address' => 'required | max:64',
+               'recruit' => 'required',
           ]);
+
           // 店舗登録処理
-          $this->storeService->storeEdit($request,$store_id);
+          $this->storeService->storeEdit($request, $store_id);
           return redirect()->route('admin/store');
      }
+
      /**
-     * 店舗削除
-     */
-     public function deletePost(Request $request, $store_id){
-     // 店舗登録処理
-     $this->storeService->storeDelete($request,$store_id);
-     return redirect()->route('admin/store');
-    }
+      * 店舗削除
+      * @param request  削除データ
+      * @param store_id  対象店舗ID
+      */
+     public function deletePost($store_id)
+     {
+          // 店舗登録処理
+          $this->storeService->storeDelete($store_id);
+          return redirect()->route('admin/store');
+     }
+
      /**
       * 店舗詳細
+      * @param store_id  対象店舗ID
       */
-     public function detail(Request $request, $store_id){
-          // user取得
-          $commonController = new CommonController();
+     public function detail($store_id)
+     {
+          $store = $this->commonController->getStore($store_id);
+          $store['postNumPrev'] = substr($store['post_num'], 0, 3);
+          $store['postNumNext'] = substr($store['post_num'], 3, 7);
+          $prefecture = $this->commonController->getPrefecture($store->prefecture_id);
+
           // sessionユーザIDを取得
-          $user_id = session()->get('user_id');
-          $user = $commonController->getUser($user_id);
-          $store = $commonController->getStore($store_id);
-          $store['postNumPrev'] = substr($store['post_num'],0,3);
-          $store['postNumNext'] = substr($store['post_num'],3,7);
-          $prefecture = $commonController->getPrefecture($store->prefecture_id);
-          return view('admin.store.detail',['user'=>$user,'store'=>$store,'prefecture'=>$prefecture]);
+          $user = $this->commonController->getUser($this->user_id);
+          return view('admin.store.detail', ['user' => $user, 'store' => $store, 'prefecture' => $prefecture]);
      }
 }
--- app/Services/StoreService.php
+++ app/Services/StoreService.php
@@ -4,15 +4,18 @@ namespace App\Services;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 // models
 use App\Models\Store;
+use Throwable;
+
 class StoreService
 {
 
-/**
- * 店舗登録処理
- * @param request  : post値 
- */
+  /**
+   * 店舗登録処理
+   * @param request  : post値 
+   */
   public function storeRegist(Request $request)
   {
     try {
@@ -20,7 +23,7 @@ class StoreService
       DB::beginTransaction();
       Store::create([
         'name'     => $request['storeName'],
-        'post_num'    => $request['postNumPrev'].$request['postNumNext'],
+        'post_num'    => $request['postNumPrev'] . $request['postNumNext'],
         'prefecture_id' => $request['prefecture'],
         'city' => $request['city'],
         'address' => $request['address'],
@@ -28,24 +31,25 @@ class StoreService
       ]);
       DB::commit();
       // トランザクション終了
-  } catch (Throwable $e) {
+    } catch (Throwable $e) {
+      Log::error($e);
       DB::rollBack();
+    }
+    return;
   }
-    return ;
-  }
-/**
- * 店舗編集処理
- * @param request  : post値 
- * @param store_id : 店舗ID
- */
-  public function storeEdit(Request $request,$store_id)
+  /**
+   * 店舗編集処理
+   * @param request  : post値 
+   * @param store_id : 店舗ID
+   */
+  public function storeEdit(Request $request, $store_id)
   {
     try {
       // トランザクション開始
       DB::beginTransaction();
       Store::where('id', $store_id)->update([
         'name' => $request['storeName'],
-        'post_num' => $request['postNumPrev'].$request['postNumNext'],
+        'post_num' => $request['postNumPrev'] . $request['postNumNext'],
         'prefecture_id' => $request['prefecture'],
         'city' => $request['city'],
         'address' => $request['address'],
@@ -53,17 +57,17 @@ class StoreService
       ]);
       DB::commit();
       // トランザクション終了
-  } catch (Throwable $e) {
+    } catch (Throwable $e) {
+      Log::error($e);
       DB::rollBack();
+    }
+    return;
   }
-    return ;
-  }
-/**
- * 店舗削除処理
- * @param request  : post値 
- * @param store_id : 店舗ID
- */
-  public function storeDelete(Request $request,$store_id)
+  /**
+   * 店舗削除処理
+   * @param store_id : 店舗ID
+   */
+  public function storeDelete($store_id)
   {
     try {
       // トランザクション開始
@@ -71,9 +75,10 @@ class StoreService
       Store::find($store_id)->delete();
       DB::commit();
       // トランザクション終了
-  } catch (Throwable $e) {
+    } catch (Throwable $e) {
+      Log::error($e);
       DB::rollBack();
-  }
-    return ;
+    }
+    return;
   }
 }
