a89f974d88d818ffc827084dc98a4499bd9bd704
h_kinjo <h.kinjo.11.8@gmail.com>
2023-05-02 23:25:57 +0900

    transaction add

--- app/Services/AlertService.php
+++ app/Services/AlertService.php
@@ -29,34 +29,35 @@ class AlertService
    */
   public function alertRegist(Request $request)
   {
-    $file_name = null;
-    $file_path = null;
-    if ($request->file('image')) {
-      $file_name = $request->file('image')->getClientOriginalName();
-      // file保存処理
-      $commonController = new CommonController();
-      $file_path = $commonController->saveFile($request->file('image'), 'alert');
-    }
-
-    // 現在日時取得
-    $now = Carbon::now();
-
+    Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' start ');
     try {
       // トランザクション開始
-      DB::beginTransaction();
-      Alert::create([
-        'category_id'  => $request['category'],
-        'title'    => $request['postNumPrev'] . $request['title'],
-        'body' => $request['body'],
-        'file_name' => $file_name,
-        'file_path' => $file_path,
-        'posted_date' => $now,
-      ]);
-      DB::commit();
-      // トランザクション終了
+      DB::transaction(function () use ($request) {
+        $file_name = null;
+        $file_path = null;
+        if ($request->file('image')) {
+          $file_name = $request->file('image')->getClientOriginalName();
+          // file保存処理
+          $commonController = new CommonController();
+          $file_path = $commonController->saveFile($request->file('image'), 'alert');
+        }
+
+        // 現在日時取得
+        $now = Carbon::now();
+
+        Alert::create([
+          'category_id'  => $request['category'],
+          'title'    => $request['postNumPrev'] . $request['title'],
+          'body' => $request['body'],
+          'file_name' => $file_name,
+          'file_path' => $file_path,
+          'posted_date' => $now,
+        ]);
+      });
+      Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' success ');
     } catch (Exception $e) {
-      DB::rollBack();
-      Log::info($e);
+      Log::error(__CLASS__ . ' ' . __FUNCTION__ . ' error ');
+      Log::error($e);
     }
     return;
   }
