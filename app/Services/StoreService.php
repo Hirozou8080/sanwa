56ffac354bce99eb43801eb815fa3cd55c0b28c5
h_kinjo <h.kinjo.11.8@gmail.com>
2023-05-03 17:47:34 +0900

    StoreService add transaction

--- app/Services/StoreService.php
+++ app/Services/StoreService.php
@@ -18,22 +18,25 @@ class StoreService
    */
   public function storeRegist(Request $request)
   {
+    Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' start');
+
     try {
       // トランザクション開始
-      DB::beginTransaction();
-      Store::create([
-        'name'     => $request['storeName'],
-        'post_num'    => $request['postNumPrev'] . $request['postNumNext'],
-        'prefecture_id' => $request['prefecture'],
-        'city' => $request['city'],
-        'address' => $request['address'],
-        'recruit_flg' => $request['recruit']
-      ]);
-      DB::commit();
-      // トランザクション終了
+      DB::transaction(function () use ($request) {
+
+        Store::create([
+          'name'     => $request['storeName'],
+          'post_num'    => $request['postNumPrev'] . $request['postNumNext'],
+          'prefecture_id' => $request['prefecture'],
+          'city' => $request['city'],
+          'address' => $request['address'],
+          'recruit_flg' => $request['recruit']
+        ]);
+        Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' success');
+      });
     } catch (Throwable $e) {
+      Log::error(__CLASS__ . ' ' . __FUNCTION__ . ' error');
       Log::error($e);
-      DB::rollBack();
     }
     return;
   }
@@ -44,40 +47,49 @@ class StoreService
    */
   public function storeEdit(Request $request, $store_id)
   {
+    Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' start');
+
     try {
       // トランザクション開始
-      DB::beginTransaction();
-      Store::where('id', $store_id)->update([
-        'name' => $request['storeName'],
-        'post_num' => $request['postNumPrev'] . $request['postNumNext'],
-        'prefecture_id' => $request['prefecture'],
-        'city' => $request['city'],
-        'address' => $request['address'],
-        'recruit_flg' => $request['recruit']
-      ]);
-      DB::commit();
-      // トランザクション終了
+      DB::transaction(function () use ($request, $store_id) {
+        // 該当の店舗の更新
+        Store::where('id', $store_id)
+          ->update([
+            'name' => $request['storeName'],
+            'post_num' => $request['postNumPrev'] . $request['postNumNext'],
+            'prefecture_id' => $request['prefecture'],
+            'city' => $request['city'],
+            'address' => $request['address'],
+            'recruit_flg' => $request['recruit']
+          ]);
+      });
+      Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' success');
     } catch (Throwable $e) {
+      Log::error(__CLASS__ . ' ' . __FUNCTION__ . ' error');
       Log::error($e);
-      DB::rollBack();
     }
+
     return;
   }
+
   /**
    * 店舗削除処理
    * @param store_id : 店舗ID
    */
   public function storeDelete($store_id)
   {
+    Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' start');
+
     try {
       // トランザクション開始
-      DB::beginTransaction();
-      Store::find($store_id)->delete();
-      DB::commit();
-      // トランザクション終了
+      DB::transaction(function () use ($store_id) {
+        // 該当店舗の削除
+        Store::find($store_id)->delete();
+      });
+      Log::info(__CLASS__ . ' ' . __FUNCTION__ . ' success');
     } catch (Throwable $e) {
+      Log::error(__CLASS__ . ' ' . __FUNCTION__ . ' error');
       Log::error($e);
-      DB::rollBack();
     }
     return;
   }
