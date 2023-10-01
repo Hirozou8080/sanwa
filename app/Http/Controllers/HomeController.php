afd0ef4c5271ffe58649d179f75e2319330b871d
h_kinjo <h.kinjo.11.8@gmail.com>
2023-10-01 19:11:29 +0900

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function home(Request $request){
-        return view("home");
-    }
+  public function home()
+  {
+    return view("home");
+  }
 }
