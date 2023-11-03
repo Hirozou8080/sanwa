ad0a7bff60407cf27c934434cc36720c0d0f4f8f
h_kinjo <h.kinjo.11.8@gmail.com>
2023-11-03 11:27:33 +0900

    ailias fix

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,14 +8,15 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function home(Request $request){
-        
+    public function home(Request $request)
+    {
+
         // user取得
         $commonController = new CommonController();
         // sessionユーザIDを取得
         $user_id = session()->get('user_id');
         $user = $commonController->getUser($user_id);
 
-        return view('admin.home',['user'=>$user]);
+        return view('admin.home', ['user' => $user]);
     }
 }
--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\CommonController;
 use App\Services\StoreService;
